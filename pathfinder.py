e6126324fd57c5025e898cdeef2e35de6711c99a
Hung Huang <hung_h102@icloud.com>
2025-04-02 22:36:07 +1030

    savin

--- pathfinder.py
+++ pathfinder.py
@@ -9,22 +9,74 @@ DEGREE = 'UG' # or PG if you are in the postgraduate course
 def parseMap(filePath):
     with open(filePath, 'r') as f:
         lines = f.readLines()
-    rows, cols = map(int, lines[0].split())
-    start = tuple(map(int, lines[1].split))
-    goal = tuple(map(int, lines[2].split))
-    map = []
+    row, col = map(int, lines[0].split()) 
+    start = tuple(map(int, lines[1].split)) # (row,col)
+    goal = tuple(map(int, lines[2].split)) # (row,col)
+    map = [] # [(line1), (line2), (line3), ...]
     for line in lines[3:]:
         map.append([int(i) if i != 'X' else 'X' for i in line.split()])
-    return rows, cols, start, goal, map
+    return row, col, start, goal, map
 
-def getMoves():
+def getMoves(x, y, map):
+    row, col = len(map), len(map[0])
+    formula = [(1,0), (-1,0), (0,1), (0,-1)]
+    moves = []
 
-def getCost():
+    for nx, ny in formula:
+        nx, ny = nx + x, ny + y
+        if 0 <= nx < row and 0 <= ny < col and map[nx][ny] != 'X':
+            moves.append((nx,ny))
 
+def getCost(curr, next):
+    elevation = next - curr
+    if elevation > 0:
+        return 1 + elevation
+    else:
+        return 1
+         
 def bfs(start, goal, map):
     visited = set()
-    queue = [(start, 0, [start])] # (position, cost, path)
+    queue = [(start, 0, [start])] # (position, path)
+    while queue:
+        (row,col), path = queue.pop(0)
+        if (row,col) == goal:
+            return path
+        if (row,col) in visited:
+            continue
+        visited.add((row,col))
+        for nx,ny in getMoves(row, col, map):
+            queue.append((nx,ny), path + [(nx,ny)])
+    return None
 
+# EDIT THIS
+def ucs(start, goal, map):
+    visited = set()
+    queue = [(start, 0, [start])] # (position, path)
+    while queue:
+        (row,col), path = queue.pop(0)
+        if (row,col) == goal:
+            return path
+        if (row,col) in visited:
+            continue
+        visited.add((row,col))
+        for nx,ny in getMoves(row, col, map):
+            queue.append((nx,ny), path + [(nx,ny)])
+    return None
+
+# EDIT THIS
+def astar(start, goal, map):
+    visited = set()
+    queue = [(start, 0, [start])] # (position, path)
+    while queue:
+        (row,col), path = queue.pop(0)
+        if (row,col) == goal:
+            return path
+        if (row,col) in visited:
+            continue
+        visited.add((row,col))
+        for nx,ny in getMoves(row, col, map):
+            queue.append((nx,ny), path + [(nx,ny)])
+    return None
 
 def main():
     mode = sys.argv[1]
@@ -43,6 +95,7 @@ def main():
         print("invalid, choose bfs, ucs, or astar")
         sys.exit(1)
     
+    # EDIT THIS
     if path:
         print("path:\n", path)
     else:
